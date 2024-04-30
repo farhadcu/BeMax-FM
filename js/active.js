d45cb9af910d606b25a73373df8c25454ca8376f
farhadcu <fmfarhad231@gmail.com>
2024-05-01 00:22:33 +0600

    video added

--- js/active.js
+++ js/active.js
@@ -1,36 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    /* slider-active */
     $('.slider-active').owlCarousel({
-        loop:true,
+        loop: true,
         // margin:10,
-        nav:true,
-        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
-        responsive:{
-            0:{
-                items:1
+        nav: true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     })
 
+    /* project-active */
     $('.project-active').owlCarousel({
-        loop:true,
-        nav:false,
-        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        nav: false,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
     })
+
+
+    /* magnificPopup-active */
+    $('.video-popup').magnificPopup({
+        // type: 'image'
+        type: 'iframe'
+        // other options
+    });
+
+
 })
